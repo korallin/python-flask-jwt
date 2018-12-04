1e56c37d28d978c9a7c3b46fd6b346d315f06d78
tiagosleal <tiagoleal.ssa@gmail.com>
2018-12-04 21:06:13 -0200

    Alteracoes diversas

--- teste.py
+++ teste.py
@@ -6,22 +6,20 @@ import time
 def __remove_acento(texto):
     return normalize('NFKD', texto.decode('utf-8')).encode('ASCII', 'ignore')
 
-def capitaliza_nome_pessoa(nome_completo):
+def capitaliza_nome_pessoa_antigo(nome_completo):
     excecao = ['dos','da','do','de']
     nome_capitalizado = ''
     nome_completo = __remove_acento(nome_completo)
-    for n in nome_completo.lower().split(' '):        
+    for n in nome_completo.lower().split(' '):
         if n not in excecao:
             n = n.capitalize()
         nome_capitalizado += n + " "
     return nome_capitalizado.strip()
 
-start_time = time.time()
-print capitaliza_nome_pessoa("josé araújo")
-print("--- %s seconds ---" % (time.time() - start_time))
+def normaliza_nome_pessoa(nome_completo):
+    nome_completo = normalize('NFKD', nome_completo.decode('utf-8')).encode('ASCII', 'ignore')
+    excecao = ['dos','da','do','de']
+    lista = [x.capitalize() if x not in excecao else x for x in nome_completo.split()]
+    return ' '.join(lista)
 
-start_time = time.time()
-excecao = ['do']
-lista = [x.capitalize() if x not in excecao else x for x in __remove_acento("josé araújo").split()]
-print ' '.join(lista)
-print("--- %s seconds ---" % (time.time() - start_time))
+print normaliza_nome_pessoa("josé de araújo mota")
